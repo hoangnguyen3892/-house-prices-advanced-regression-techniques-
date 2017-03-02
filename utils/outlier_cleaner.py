83e7c32539fd7cd2efd8be660455b1cb683f4fe5
hoangnguyen3892 <hoangnguyen3892@gmail.com>
2017-03-02 10:47:18 +0900

    Submission

--- utils/outlier_cleaner.py
+++ utils/outlier_cleaner.py
@@ -1,18 +1,20 @@
-def outlierCleaner(predictions, ages, net_worths):
+def outlierCleaner(predictions, x, y):
     """
-        clean away the 10% of points that have the largest
+        Clean away 5 points that have the largest
         residual errors (different between the prediction
-        and the actual net worth)
-        return a list of tuples named cleaned_data where 
-        each tuple is of the form (age, net_worth, error)
+        and the actual)
+
+        Return a list of tuples named cleaned_data where 
+        each tuple is of the form (x, y, error)
     """
     
-    cleaned_data = list(zip(ages, net_worths, [(float(pred) - actual)**2 for pred, actual in zip(predictions, net_worths)]))
+    cleaned_data = list(zip(x, y, [(float(pred) - actual)**2 for pred, actual in zip(predictions, y)]))
 
     cleaned_data.sort(key = lambda tup: tup[2])
 
     #for i in range(0, int(len(cleaned_data) * 0.1)):
-    for i in range(0, 3):
+    cleaned_list = []
+    for i in range(0, 5):
+        cleaned_list.append(cleaned_data[-1][0])
         cleaned_data.pop()
-    
-    return cleaned_data
+    return cleaned_data, cleaned_list
